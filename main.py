61ff2d0015d9f96ab325aa5ce6f1a9187663376c
Kenjidoom <vitodevcool@protonmail.com>
2023-04-17 18:20:23 -0700

    entry box added

--- main.py
+++ main.py
@@ -9,14 +9,17 @@ class Shomap(customtkinter.CTk):
         self.title("Shomaps")
 
         pw_windows = Panedwindow(self, orient='horizontal')
-        Panel1_results = LabelFrame(pw_windows, text="Shodan Results")
+        Panel1_results = LabelFrame(pw_windows, text="Shodan Results", height=750)
         panel2_maps = LabelFrame(pw_windows, text="Shodan Maps")
         pw_windows.add(Panel1_results, weight=50)
         pw_windows.add(panel2_maps, weight=50)
-        pw_windows.pack(fill='both', expand=True)
+        pw_windows.pack(fill='both', expand=False)
+
+        # Entry box needed
+        IP_Entry = customtkinter.CTkEntry(master=self, placeholder_text="Enter IP Address",placeholder_text_color=('black'), height=40, width=1000).place(x=700, y=775, anchor='center')
 
 
 app = Shomap()
-app.configure(fg_color='black')
+app.configure(fg_color='grey')
 app.resizable(False, False)
 app.mainloop()
