487fb21945a37387539f358bfb5d6bc76479d220
Kenjidoom <vitodevcool@protonmail.com>
2023-04-17 17:32:43 -0700

    Paned windows added for results and maps

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import customtkinter
+from tkinter import *
+from tkinter.ttk import *
 
 class Shomap(customtkinter.CTk):
     def __init__(self):
@@ -6,8 +8,15 @@ class Shomap(customtkinter.CTk):
         self.geometry("1400x800")
         self.title("Shomaps")
 
+        pw_windows = Panedwindow(self, orient='horizontal')
+        Panel1_results = LabelFrame(pw_windows, text="Shodan Results")
+        panel2_maps = LabelFrame(pw_windows, text="Shodan Maps")
+        pw_windows.add(Panel1_results, weight=50)
+        pw_windows.add(panel2_maps, weight=50)
+        pw_windows.pack(fill='both', expand=True)
+
 
 app = Shomap()
-app.configure(fg_color='grey')
+app.configure(fg_color='black')
 app.resizable(False, False)
 app.mainloop()
