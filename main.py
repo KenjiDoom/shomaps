96ed6ef37ce660b8cdafeda0099bb1c89f4bd8d8
Kenjidoom <vitodevcool@protonmail.com>
2023-04-18 21:12:08 -0700

    map added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import customtkinter
+import tkintermapview
 from tkinter import *
 from tkinter.ttk import *
 import shodan
@@ -14,19 +15,18 @@ class Shomap(customtkinter.CTk):
 
         pw_windows = Panedwindow(self, orient='horizontal')
         self.Panel1_results = LabelFrame(pw_windows, text="Shodan Results", height=750)
-        panel2_maps = LabelFrame(pw_windows, text="Shodan Maps")
+        self.panel2_maps = LabelFrame(pw_windows, text="Shodan Maps")
         pw_windows.add(self.Panel1_results, weight=50)
-        pw_windows.add(panel2_maps, weight=50)
+        pw_windows.add(self.panel2_maps, weight=50)
         pw_windows.pack(fill='both', expand=True)
 
         # Entry box needed
         IP_Entry = customtkinter.CTkEntry(master=self, placeholder_text="Enter IP Address",placeholder_text_color=('black'), height=40, width=900).place(x=700, y=775, anchor='center')
         search_button = customtkinter.CTkButton(master=self, text='Search', command=self.shodan_search).place(x=1250, y=775, anchor='center') # Don't forget add the command
-
-        # Add the for loop here?
-        # label = customtkinter.CTkLabel(master=Panel1_results, text='Testing Label')
-        # label.pack()
-
+        
+        map_wdiget = tkintermapview.TkinterMapView(self.panel2_maps, width=200, height=200, corner_radius=10)
+        map_wdiget.pack()
+    
     def shodan_search(self):
         # IP fixed for now
         IP = ''
@@ -45,6 +45,10 @@ class Shomap(customtkinter.CTk):
         for index, (a, b, c, d, e, f, g, h) in enumerate(zip(ip, banner, port, city, domains, asn, coordinates, coordinates2)):
             label = customtkinter.CTkLabel(master=self.Panel1_results, text=(str(a), str(b), str(c), str(d), str(e), str(f), str(g), str(h)))
             label.pack()
+
+    def shodan_map(self):
+        map_wdiget = tkintermapview.TkinterMapView(self.panel2_maps, width=10, height=10, corner_radius=0)
+        map_wdiget.pack()
 
 app = Shomap()
 app.configure(fg_color='grey')
