137fa41888e2e7a2fe741a6e860e903f9ccd10d1
kenjidoom <77996865+KenjiDoom@users.noreply.github.com>
2023-04-19 17:22:55 -0700

    removed my password lol

--- main.py
+++ main.py
@@ -14,19 +14,26 @@ class Shomap(customtkinter.CTk):
         self.title("Shomaps")
 
         pw_windows = Panedwindow(self, orient='horizontal')
-        self.Panel1_results = LabelFrame(pw_windows, text="Shodan Results", height=750)
-        self.panel2_maps = LabelFrame(pw_windows, text="Shodan Maps")
+        self.Panel1_results = LabelFrame(pw_windows, text="General Information")
+        #self.Panel1_results = customtkinter.CTkFrame(master=pw_windows, fg_color='white')
+        #self.panel2_maps = customtkinter.CTkFrame(master=pw_windows, fg_color='white')
+        
+        self.panel2_maps = LabelFrame(pw_windows, text="Shodan Maps", relief='solid', borderwidth=1)
+        
         pw_windows.add(self.Panel1_results, weight=50)
         pw_windows.add(self.panel2_maps, weight=50)
         pw_windows.pack(fill='both', expand=True)
 
         # Entry box needed
-        IP_Entry = customtkinter.CTkEntry(master=self, placeholder_text="Enter IP Address",placeholder_text_color=('black'), height=40, width=900).place(x=700, y=775, anchor='center')
+        IP_Entry = customtkinter.CTkEntry(master=self, placeholder_text="Enter IP Address", placeholder_text_color=('black'), height=40, width=900).place(x=700, y=775, anchor='center')
         search_button = customtkinter.CTkButton(master=self, text='Search', command=self.shodan_search).place(x=1250, y=775, anchor='center') # Don't forget add the command
         
-        map_wdiget = tkintermapview.TkinterMapView(self.panel2_maps, width=200, height=200, corner_radius=10)
-        map_wdiget.pack()
-    
+                                                        # Change this to frame
+        map_widget = tkintermapview.TkinterMapView(self.panel2_maps, corner_radius=0)
+        map_widget.grid(row=0, column=0, sticky="nsew") # This method might not work for our output characters
+        self.panel2_maps.grid_rowconfigure(0, weight=1)
+        self.panel2_maps.grid_columnconfigure(0, weight=1)
+
     def shodan_search(self):
         # IP fixed for now
         IP = ''
@@ -43,12 +50,11 @@ class Shomap(customtkinter.CTk):
             coordinates2.append(items['location']['longitude'])
 
         for index, (a, b, c, d, e, f, g, h) in enumerate(zip(ip, banner, port, city, domains, asn, coordinates, coordinates2)):
-            label = customtkinter.CTkLabel(master=self.Panel1_results, text=(str(a), str(b), str(c), str(d), str(e), str(f), str(g), str(h)))
-            label.pack()
+                                                # Change this to frame
+            #results = customtkinter.CTkLabel(master=self.frame_results, text=(str(a), str(b), str(c), str(d), str(e), str(f), str(g), str(h))).place(x=10)
+            IP_label = customtkinter.CTkLabel(master=self.Panel1_results, text=(str(a))).place(x=10, y=20)
 
-    def shodan_map(self):
-        map_wdiget = tkintermapview.TkinterMapView(self.panel2_maps, width=10, height=10, corner_radius=0)
-        map_wdiget.pack()
+
 
 app = Shomap()
 app.configure(fg_color='grey')
