9d2f4aa0cc9c8e92ec4011531eaa9be18a6c2361
Kenjidoom <vitodevcool@protonmail.com>
2023-04-22 19:38:26 -0700

    comments removed and icon image added

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+from tkinter.ttk import *
+from tkinter import *
 import customtkinter
 import tkintermapview
-from tkinter import *
-from tkinter.ttk import *
 import shodan
-# Add your key here
+
 api = shodan.Shodan('')
 
 
@@ -15,19 +15,18 @@ class Shomap(customtkinter.CTk):
 
         pw_windows = Panedwindow(self, orient='horizontal')
         self.Panel1_results = LabelFrame(pw_windows, text="General Information")
-        #self.Panel1_results = customtkinter.CTkFrame(master=pw_windows, fg_color='white')
-        #self.panel2_maps = customtkinter.CTkFrame(master=pw_windows, fg_color='white')
-        
         self.panel2_maps = LabelFrame(pw_windows, text="Shodan Maps", relief='solid', borderwidth=1)
         
         pw_windows.add(self.Panel1_results, weight=50)
         pw_windows.add(self.panel2_maps, weight=50)
         pw_windows.pack(fill='both', expand=True)
-
-        # Entry box needed
+        
         IP_Entry = customtkinter.CTkEntry(master=self, placeholder_text="Enter IP Address", placeholder_text_color=('black'), height=40, width=900)
         IP_Entry.place(x=700, y=775, anchor='center')
         search_button = customtkinter.CTkButton(master=self, text='Search', command=lambda:[self.shodan_search(str(IP_Entry.get())), self.display_map(IP_Entry.get())]).place(x=1250, y=775, anchor='center') 
+        
+        self.icon_image = PhotoImage(file='/home/kenji/Documents/shomaps/assets/shodan-icon.png')
+        self.iconphoto(False, self.icon_image)
     
     def shodan_search(self, IP):
         host = api.host(IP)
@@ -43,13 +42,12 @@ class Shomap(customtkinter.CTk):
             coordinates2.append(items['location']['longitude'])
         
         for index, (a, b, c, d, e, f, g, h) in enumerate(zip(ip, banner, port, city, domains, asn, coordinates, coordinates2)):
-            #results = customtkinter.CTkLabel(master=self.frame_results, text=(str(a), str(b), str(c), str(d), str(e), str(f), str(g), str(h))).place(x=10)
             IP_label = customtkinter.CTkLabel(master=self.Panel1_results, text=(str(a))).place(x=10, y=20)
-           # BANNER_label = customtkinter.CTkLabel(master=self.Panel1_results, text=(str(b))).place(x=10, y=50)
-            #PORT_label = customtkinter.CTkLabel(master=self.Panel1_results, text=(str(c))).place(x=10, y=150)
-            #CITY_label = customtkinter.CTkLabel(master=self.Panel1_results, text=(str(d))).place(x=10, y=200)
-            #DOMAINS_label = customtkinter.CTkLabel(master=self.Panel1_results, text=(str(e))).place(x=10, y=250)
-            #ASN_label = customtkinter.CTkLabel(master=self.Panel1_results, text=(str(f))).place(x=10, y=300)
+            BANNER_label = customtkinter.CTkLabel(master=self.Panel1_results, text=(str(b))).place(x=10, y=50)
+            PORT_label = customtkinter.CTkLabel(master=self.Panel1_results, text=(str(c))).place(x=10, y=150)
+            CITY_label = customtkinter.CTkLabel(master=self.Panel1_results, text=(str(d))).place(x=10, y=200)
+            DOMAINS_label = customtkinter.CTkLabel(master=self.Panel1_results, text=(str(e))).place(x=10, y=250)
+            ASN_label = customtkinter.CTkLabel(master=self.Panel1_results, text=(str(f))).place(x=10, y=300)
             COORDINATES_label = customtkinter.CTkLabel(master=self.Panel1_results, text=(str(g))).place(x=10, y=350)
             COORDINATES2_label = customtkinter.CTkLabel(master=self.Panel1_results, text=(str(h))).place(x=70, y=350)
 
@@ -62,10 +60,10 @@ class Shomap(customtkinter.CTk):
             print(self.coordinates, self.coordinates2)
         for index, (a, b) in enumerate(zip(self.coordinates, self.coordinates2)):
             self.map_widget = tkintermapview.TkinterMapView(self.panel2_maps, corner_radius=0)
-            self.map_widget.set_position(a, b) # Pass coorindates Variables here
-            self.map_widget.set_marker(a, b, text=str(IP)) # pass Variables into here
-            self.map_widget.set_tile_server("https://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}&s=Ga", max_zoom=22) # Using google servers
-            self.map_widget.grid(row=0, column=0, sticky="nsew") # This method might not work for our output characters
+            self.map_widget.set_position(a, b)
+            self.map_widget.set_marker(a, b, text=str(IP))
+            self.map_widget.set_tile_server("https://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}&s=Ga", max_zoom=22)
+            self.map_widget.grid(row=0, column=0, sticky="nsew") 
 
             self.panel2_maps.grid_rowconfigure(0, weight=1)
             self.panel2_maps.grid_columnconfigure(0, weight=1)
