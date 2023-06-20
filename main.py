e3651711ccac4bfc40c883027cde03deb882a293
KenjiDoom <77996865+KenjiDoom@users.noreply.github.com>
2023-06-20 15:30:09 -0700

    Removed a lot of headaches

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import shodan, nmap3, tkintermapview, customtkinter, threading, time, os, sys, dns.resolver, socket, re, json, logging, requests, asyncio, aiohttp
+import shodan, nmap3, tkintermapview, customtkinter, threading, time, os, sys, re, json, requests, asyncio, aiohttp
 from tkinter import IntVar, Checkbutton, Button, Label, Frame, Scrollbar, messagebox, simpledialog
 from multiprocessing import Process
 from dotenv import dotenv_values
 from tkinter.ttk import *
 from tkinter import *
-
-from concurrent.futures import ThreadPoolExecutor
 
 def check_Root():
     if os.geteuid() == 0:
@@ -39,91 +37,7 @@ def check_API():
 
 global api_key
 api_key = ''
-
-# ------------------ More Information ------------$
-class moreInfo(customtkinter.CTk):
-    IP = ''
-    
-    def __init__(self, IP):
-        super().__init__()
-        self.geometry('600x600')
-        self.title('More Information')
-        
-        self.IP = IP
-        
-        pd_windows = Panedwindow(master=self, orient='vertical')
-
-        # Frame windows
-        Panel1_CVE = LabelFrame(pd_windows, relief='flat', text='CVE Information: ' + self.IP, background='white')
-        Panel2_DNS = LabelFrame(pd_windows, relief='flat', text='DNS Information: ' + self.IP, background='white')
-
-        # CVE Text
-        self.cve_text = Text(master=Panel1_CVE, background='white')
-        self.cve_text.place(x=20, y=20)
-        
-        # DNS Label
-        self.dns_label = Label(master=Panel2_DNS, text='', background='white')
-        self.dns_label.place(x=50, y=50)
-
-        # FRame windows extension
-        pd_windows.add(Panel1_CVE, weight=50)
-        pd_windows.add(Panel2_DNS, weight=50)
-        pd_windows.pack(fill='both', expand=True)
-
-        self.resizable(False, False)
-    
-    async def fetch_multiple(self, session, urls):
-        print('Fetch Multiple is running')
-        tasks = [self.fetch(session, url) for url in urls]
-        responses = await asyncio.gather(*tasks)
-        return responses
-
-    async def fetch(self, session, url):
-        print('Fetch is running')
-        async with session.get(url) as response:
-            return await response.json()
-
-    async def cve_info(self, IP):
-        output = []
-        response_data = []
-        host = api.host(str(IP))
-        cve_names = [cve for cve in host['vulns'][0:5]] # Limit to only 5 searches (5 cves)
-        urls = ['http://api.cvesearch.com/search?q=' + item for item in cve_names]
-
-        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(limit=None)) as session:
-            responses = await self.fetch_multiple(session, urls)
-            for response in responses: # this is being converted into a dict.
-                response_data = response
-                for cve in cve_names:
-                    try:
-                        print(str(cve) + response_data['response'][str(cve).lower()]['basic']['description'] + '\n') 
-                        output.append(str(cve) + str(response_data['response'][str(cve).lower()]['basic']['description']) + '\n')
-                    except KeyError:
-                        pass
-            self.cve_text.insert(END, ''.join(output))
-    
-    def dns_info(self):
-        def enum_code(domain):
-            try:
-               for qtype in 'A', 'AAAA', 'MX', 'NS', 'PTR':
-                   answer = dns.resolver.resolve(domain, qtype, raise_on_no_answer=False)
-                   if answer.rrset is not None:
-                       output = str(answer.rrset) + '\n'
-                       self.dns_label.config(text=output)
-                       print(output)
-            except dns.resolver.NXDOMAIN:
-               print('The domain entered doesnt exist, enter your own maunaly') 
-        
-        data = socket.gethostbyaddr(self.IP)
-        domain_name = data[0]
-        popup = messagebox.askquestion(title='Right or wrong?', message=f"Is this the right domain? {domain_name}")
-        if popup == 'yes':
-            enum_code(domain_name) 
-        elif popup == 'no':
-            target = customtkinter.CTkInputDialog(text='Enter domain name:', title='Domain')
-            domain = target.get_input()
-            enum_code(domain)
-
+   
 # ------------------ Namp window ----------------- #
 
 class Nwindow(customtkinter.CTk):
@@ -224,8 +138,6 @@ class Nwindow(customtkinter.CTk):
         with open(str(IP) + scan_type + '.json', 'w') as f:
             f.write(json_object)
             f.close()
-# Side-note: Write a function to do all the file writing instead duplicating all the code...
-
 # ------------------ Namp window ----------------- #
 
 
@@ -233,15 +145,49 @@ class Nwindow(customtkinter.CTk):
 
 def loading_bar(IP):
     progress = Progressbar(root, mode="determinate", length=150)
-    progress.place(x=1200, y=765)
+    progress.place(x=820, y=765)
     progress['value'] = 50
     progress.update_idletasks()
     time.sleep(1)
     P10 = Process(target=shodan_search(IP))
     P10.start()
+    loop = asyncio.new_event_loop()
+    loop.run_until_complete(cve_info(IP))
     progress['value'] = 100
     progress.update_idletasks
     time.sleep(1)
+
+async def fetch_multiple(session, urls):
+    print('Fetch Multiple is running')
+    tasks = [fetch(session, url) for url in urls]
+    responses = await asyncio.gather(*tasks)
+    return responses
+
+async def fetch(session, url):
+    print('Fetch is running')
+    async with session.get(url) as response:
+        return await response.json()
+
+async def cve_info(IP):
+    output = []
+    response_data = []
+    host = api.host(str(IP))
+    cve_names = [cve for cve in host['vulns'][0:5]] # Limit to only 5 searches (5 cves)
+    urls = ['http://api.cvesearch.com/search?q=' + item for item in cve_names]
+    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(limit=None)) as session:
+        responses = await fetch_multiple(session, urls)
+        for response in responses: # this is being converted into a dict.
+            response_data = response
+            for cve in cve_names:
+                try:
+                    print(str(cve) + response_data['response'][str(cve).lower()]['basic']['description'] + '\n') 
+                    output.append(str(cve) + str(response_data['response'][str(cve).lower()]['basic']['description']) + '\n')
+                except KeyError:
+                    pass
+        # Add to frame here
+        cve_text = Text(Panel3_information, background='white')
+        cve_text.pack(fill="both", expand=True)
+        cve_text.insert(END, ''.join(output))
 
 def shodan_search(IP):
     print('Shodan search running')
@@ -297,48 +243,42 @@ def display_shodan_map(IP):
 def nmap_window(): # Nmap window
     P1 = Nwindow(IP_Entry.get())
     P1.mainloop()
-
-def window_more():
-    IP = IP_Entry.get()
-    P2 = moreInfo(IP)
-    start_time = time.time()
-    loop = asyncio.new_event_loop()
-    loop.run_until_complete(P2.cve_info(IP))
-    print("--- %s seconds ---" % (time.time() - start_time))
-    #P2.dns_info()
-    P2.mainloop()
-
-T1 = threading.Thread(target=nmap_window, daemon=True)
-T2 = threading.Thread(target=window_more, daemon=True)
+T1 = threading.Thread(target=nmap_window, daemon=True)  
 # ------------------------------- Main Window Bellow --------------------------------------------
 
 root = Tk()
 print('Main window running')
-root.geometry("1400x800")
+root.geometry("1000x800")
 root.title("Shomaps")
 fpack = ("MS Serif", 20)
 
 # Framed windows
 pw_windows = Panedwindow(root, orient='horizontal')
-Panel1_results = LabelFrame(pw_windows, font=fpack, relief='flat', text="General Information", background='white')
+Panel1_results = LabelFrame(pw_windows, font=fpack, relief='flat', text="General Information", background='white', height=440)
 Panel2_maps = LabelFrame(pw_windows, font=fpack, text="Map", relief='flat', background='white')
+
 # Frame windows extension
 pw_windows.add(Panel1_results, weight=50)
 pw_windows.add(Panel2_maps, weight=50)
-pw_windows.pack(fill='both', expand=True)
+pw_windows.pack(fill='both', expand=False)
 
+# Veritcal frame windows - More information window
+pg_windows = Panedwindow(root, orient='vertical')
+Panel3_information = LabelFrame(pg_windows, font=fpack, text="More Information", relief='flat', background='white', height=350)
+pg_windows.add(Panel3_information, weight=50)
+pg_windows.pack(side=BOTTOM, fill='x', expand=False)
 
+# Entry box
 IP_Entry = Entry(master=root, text="Enter IP Address", width=30)
-IP_Entry.place(x=700, y=775, anchor='center')
+IP_Entry.place(x=540, y=775, anchor='center')
 
+# Search button
 search_button = Button(master=root, text='Shodan Search', command=lambda:[loading_bar(IP_Entry.get()), display_shodan_map(IP_Entry.get())])
-search_button.place(x=980, y=775, anchor='center')
+search_button.place(x=750, y=775, anchor='center')
 
+# Namp scan button
 scan_nmap = Button(master=root, text='Perform Nmap Scan', command=lambda:[T1.start()])
-scan_nmap.place(x=150, y=760)
-
-more_results = Button(master=root, text='More Information', command=lambda:[T2.start()])
-more_results.place(x=350, y=760)
+scan_nmap.place(x=250, y=760)
 
 def close_window():
     print('You clicked the Exit button')
